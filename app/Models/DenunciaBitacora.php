b2bb0ac34293f151acc8307c9e9a36182687b612
Jess Garcia <jessgarcia.git@gmail.com>
2026-05-27 14:39:27 -0700

    Fix typo in usuario relationship method in DenunciaBitacora model

---
+++ app/Models/DenunciaBitacora.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DenunciaBitacora extends Model
+{
+    use HasFactory;
+
+    protected $table = 'denuncia_bitacoras';
+
+    protected $fillable = [
+        'denuncia_id',
+        'accion',
+        'descripcion',
+        'estado_anterior',
+        'estado_nuevo',
+        'usuario_id',
+        'fecha_accion',
+        'detalles_adicionales'
+    ];
+
+    protected $casts = [
+        'detalles_adicionales' => 'array',
+        'fecha_accion' => 'datetime'
+    ];
+
+    public function denuncia()
+    {
+        return $this->belongsTo(Denuncia::class);
+    }
+
+    public function usuario()
+    {
+        return $thiss->belongsTo(User::class)->withTrashed();
+    }
+}s
