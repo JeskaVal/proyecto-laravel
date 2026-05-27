c363cc3c9ebfbf554dd3181fe1751cd6597a64d6
Jess Garcia <jessgarcia.git@gmail.com>
2026-05-27 11:21:05 -0700

    Fix formatting in cors.php configuration file

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,12 @@
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
-'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:4200', 'http://localhost:8080'],
-'allowed_origins_patterns' => [],
-'allowed_headers' => ['*'],
-'exposed_headers' => [],
-'max_age' => 0,
-'supports_credentials' => true,
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['http://localhost:4200', 'http://localhost:8080'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
