79e1f4a3a9b76027aabe195964afb852bd208796
Jess Garcia <jessgarcia.git@gmail.com>
2026-05-28 17:27:51 -0700

    Refactor EnviarActualizacionDenuncia job to include action and description parameters for email notifications

--- app/Jobs/EnviarActualizacionDenuncia.php
+++ app/Jobs/EnviarActualizacionDenuncia.php
@@ -2,7 +2,7 @@
 
 namespace App\Jobs;
 
-use App\Mail\DenunciaRecibidaMail;
+use App\Mail\ActualizacionDenunciaMail;
 use App\Models\Denuncia;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -11,17 +11,25 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
 
-class EnviarNotificacionDenuncia implements ShouldQueue
+class EnviarActualizacionDenuncia implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct(public Denuncia $denuncia) {}
+    public function __construct(
+        public Denuncia $denuncia,
+        public string $accion,
+        public string $descripcion
+        ) {}
 
     public function handle(): void
     {
         if ($this->denuncia->correo_denunciante && $this->denuncia->tipo_denunciante === 'identificado') {
             Mail::to
-            ($this->denuncia->correo_denunciante)->send(new DenunciaRecibidaMail ($this->denuncia));
+            ($this->denuncia->correo_denunciante)->send(new DenunciaRecibidaMail (
+                $this->denuncia,
+                $this->accion,
+                $this->descripcion
+                ));
         }
     }
 }
